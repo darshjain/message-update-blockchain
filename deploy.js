8b4f4abf8b74909aac707c8d2ce923c16a340cf3
darshjain <darshj786@gmail.com>
2021-06-19 22:48:26 +0530

    ENV ADDED

--- deploy.js
+++ deploy.js
@@ -1,9 +1,11 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 const { interface, bytecode } = require('./compile')
+require('dotenv/config')
+
 
 const provider = new HDWalletProvider(
-  'smoke canoe club mule retreat suspect unique abandon lion clarify dish pulse',
+   process.env.KEYPHRASE_WALLET,
   'https://rinkeby.infura.io/v3/3a4fe6fd68534f858f1f393b0a8f322d',
 )
 
@@ -12,7 +14,7 @@ const web3 = new Web3(provider)
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
     // console.log(accounts)
-    
+
   console.log('ATTEMPTING DEPLOY FROM', accounts[0])
 
    const result=  await new web3.eth.Contract(JSON.parse(interface))
