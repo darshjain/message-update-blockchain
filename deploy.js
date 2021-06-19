37b931517d0970846cd2aee43342d01299cf8d45
darshjain <darshj786@gmail.com>
2021-06-19 22:54:05 +0530

    env added

--- deploy.js
+++ deploy.js
@@ -3,23 +3,23 @@ const Web3 = require('web3')
 const { interface, bytecode } = require('./compile')
 require('dotenv/config')
 
-
 const provider = new HDWalletProvider(
-   process.env.KEYPHRASE_WALLET,
-  'https://rinkeby.infura.io/v3/3a4fe6fd68534f858f1f393b0a8f322d',
+  process.env.KEYPHRASE_WALLET,
+  process.env.INFURA_KEY,
 )
 
 const web3 = new Web3(provider)
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts()
-    // console.log(accounts)
+  // console.log(accounts)
 
   console.log('ATTEMPTING DEPLOY FROM', accounts[0])
 
-   const result=  await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['HI THERE!'] }).send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['HI THERE!'] })
+    .send({ gas: '1000000', from: accounts[0] })
 
-    console.log("CONTRACT DEPLOYED:",result.options.address);
-};
-deploy();
+  console.log('CONTRACT DEPLOYED:', result.options.address)
+}
+deploy()
